1054be24ce0fdb00fe573a9c0501a19442f5d809
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2025-11-07 23:03:38 -0500

    init pio

---
+++ esp32/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
